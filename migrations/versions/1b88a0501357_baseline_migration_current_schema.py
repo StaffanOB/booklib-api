6b7a67344c667e819123395d84e059b3a0c9ecb3
StaffanOB <staffan@toor.se>
2025-10-15 17:42:44 +0200

    add baseline migration

---
+++ migrations/versions/1b88a0501357_baseline_migration_current_schema.py
@@ -0,0 +1,24 @@
+"""baseline migration - current schema
+
+Revision ID: 1b88a0501357
+Revises: 
+Create Date: 2025-10-15 17:42:17.420894
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1b88a0501357'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
